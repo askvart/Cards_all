dd7c71bf247cc11dff8c43bd9698b23c60e43d1c
askvart <46724365+askvart@users.noreply.github.com>
2019-09-19 16:55:40 +0300

    Абстракции

---
+++ 1_abstract.py
@@ -0,0 +1,20 @@
+from abc import ABCMeta, abstractmethod
+
+class Base(metaclass=ABCMeta):
+    @abstractmethod
+    def foo(self):
+        pass
+
+    @abstractmethod
+    def bar(self):
+        pass
+
+class Concrete(Base):
+    def foo(self):
+        pass
+
+    def bar(self):
+        pass
+
+assert issubclass(Concrete, Base)
+c = Concrete()
